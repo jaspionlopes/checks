f439e03bbc10c65b1dc73fb57f7ac9131cf3d370
jaspionlopes <jaspion18@gmail.com>
2025-05-10 09:21:45 -0300

    Update local_settings.py

--- hc/local_settings.py
+++ hc/local_settings.py
@@ -1,6 +1,8 @@
+from .settings import BASE_DIR
+import os
+
 DEBUG = False
 ALLOWED_HOSTS = ["checks-ii29.onrender.com", "localhost", "127.0.0.1"]
 
-import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
