25acdb6c1782593a8ed027fadfb9c7c35235f079
jaspionlopes <jaspion18@gmail.com>
2025-05-10 09:28:28 -0300

    Update settings.py

--- hc/settings.py
+++ hc/settings.py
@@ -195,7 +195,7 @@ USE_TZ = True
 TIME_ZONE = "UTC"
 USE_I18N = False
 
-SITE_ROOT = os.getenv("SITE_ROOT", "http://localhost:8000").removesuffix("/")
+SITE_ROOT = os.getenv("SITE_ROOT", "http://checks-ii29.onrender.com:8000").removesuffix("/")
 SITE_NAME = os.getenv("SITE_NAME", "Mychecks")
 SITE_LOGO_URL = os.getenv("SITE_LOGO_URL")
 MASTER_BADGE_LABEL = os.getenv("MASTER_BADGE_LABEL", SITE_NAME)
